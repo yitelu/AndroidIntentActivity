c6aadb4aa429461abc9783852b844207a366591a
Richard Yi-Te Lu <yitelu@gmail.com>
2018-10-30 22:20:42 -0700

    Android Intent passing extra to other activity

---
+++ app/src/main/java/com/ytl/multipleactivity/SecondActivity.java
@@ -0,0 +1,28 @@
+package com.ytl.multipleactivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    public void BackToMain(View view){
+        //Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        //startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+
+    }
+}
